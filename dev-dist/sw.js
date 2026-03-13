c7f6ddf2dc9a92cf46afbbbea498944f44f0df2b
dimovdmitrii <dimovdima1509@gmail.com>
2026-03-13 21:13:38 +0100

    dev-dist enter in gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oeaqvlkkbq8"
+    "revision": "0.d33ftuku5ko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
